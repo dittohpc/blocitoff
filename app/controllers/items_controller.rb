4a163f1214449e61b8750bad02ad785d4714ec82
enoughtime@gmail.com <enoughtime@gmail.com>
2016-09-09 14:22:23 -0400

    Checkpoint Blocitoff 07 Mark Todo Items as Complete: Completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,13 @@
 class ItemsController < ApplicationController
-  def show
-  end
 
   def create
     @user = current_user
     @item = current_user.items.create(item_params)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
   end
 
   private
